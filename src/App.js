e15dba840aa31a06ab14d885b016b51fba2e5020
Omid Nasri <omid.nasri@outlook.com>
2019-01-25 10:46:26 +0330

    Creating responsive table using bootstrap

--- src/App.js
+++ src/App.js
@@ -218,45 +218,43 @@ class App extends Component {
     }
     else {
       return (
-        <div className="disable-responsive">
-          <table className="table table-striped table-bordered table-hover">
-            <thead>
-              <tr>
-                <th>ردیف</th>
-                <th>پست الکترونیکی</th>
-                <th>سرور میزبان</th>
-                <th>شماره پورت</th>
-                <th>نام کاربری</th>
-                <th>سطح دسترسی</th>
-                <th>ارتباط SSL</th>
-                <th>وضعیت سرویس تست</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                props.parent.state.dataSource.map((item, index) => {
-                  return (
-                    <tr>
-                      <td>{index + 1}</td>
-                      <td>{item.Address}</td>
-                      <td>{item.Host}</td>
-                      <td>{item.Port}</td>
-                      <td>{item.UserName}</td>
-                      <td>{item.IsPublic ? "عمومی" : "مخفی"}</td>
-                      <td>{item.EnableSsl ? "بله" : "خیر"}</td>
-                      <td>{item.Status ? "فعال" : "غیرفعال"}</td>
-                      <td>
-                        <a href="javascript:;" data-public={item.IsPublic} className={`${item.IsPublic ? "disabled-link" : ""}`} onClick={(event) => props.onEditEmail(event)} data-id={item.Id}> ویرایش </a>  |
+        <table className="table table-striped table-bordered table-hover">
+          <thead>
+            <tr>
+              <th>ردیف</th>
+              <th>پست الکترونیکی</th>
+              <th>سرور میزبان</th>
+              <th>شماره پورت</th>
+              <th>نام کاربری</th>
+              <th>سطح دسترسی</th>
+              <th>ارتباط SSL</th>
+              <th>وضعیت سرویس تست</th>
+              <th data-res="false"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              props.parent.state.dataSource.map((item, index) => {
+                return (
+                  <tr>
+                    <td>{index + 1}</td>
+                    <td>{item.Address}</td>
+                    <td>{item.Host}</td>
+                    <td>{item.Port}</td>
+                    <td>{item.UserName}</td>
+                    <td>{item.IsPublic ? "عمومی" : "مخفی"}</td>
+                    <td>{item.EnableSsl ? "بله" : "خیر"}</td>
+                    <td>{item.Status ? "فعال" : "غیرفعال"}</td>
+                    <td data-res="false">
+                      <a href="javascript:;" data-public={item.IsPublic} className={`${item.IsPublic ? "disabled-link" : ""}`} onClick={(event) => props.onEditEmail(event)} data-id={item.Id}> ویرایش </a>  |
                         <a href="javascript:;" data-public={item.IsPublic} className={`${item.IsPublic ? "disabled-link" : ""}`} onClick={(event) => props.onDeleteEmail(event)} data-id={item.Id}> حذف </a>
-                      </td>
-                    </tr>
-                  )
-                })
-              }
-            </tbody>
-          </table>
-        </div>
+                    </td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
       );
     }
   }
@@ -265,67 +263,133 @@ class App extends Component {
     return (
       <div className="row">
         <div className="col-md-12">
-          <div className="portlet light">
-            <div className="portlet-title">
-              <div className="caption">
-                <i className="fa fa-cogs font-green-sharp"></i>
-                <span className="bold caption-subject font-green-sharp">حساب کاربری پست الکترونیکی</span>
+          <div className="todo-sidebar">
+            <div className="portlet light">
+              <div className="portlet-title">
+                <div className="caption" data-toggle="collapse" data-target=".todo-project-list-content">
+                  <i className="fa fa-bars font-green-sharp"></i>
+                  <span className="caption-subject font-green-sharp bold uppercase">
+                    شبکه‌های اجتماعی
+                  </span>
+                </div>
+              </div>
+              <div className="portlet-body todo-project-list-content">
+                <div className="todo-project-list">
+                  <ul className="nav nav-pills nav-stacked">
+                    <li className="active">
+                      <a href="#">
+                        <span className="badge badge-success"> {this.state.dataSource.length} </span> تعریف حساب کاربری
+                      </a>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
-            <div className="portlet-body">
-              <div className="form-body">
-                <div className="row">
-                  <input type="hidden" name="Id" value={this.state.emailDetail.Id} />
-                  <div className={`col-md-3 ${this.errorClass(this.state.emailDetail.errors.Address)}`}>
-                    <div className="form-group">
-                      <label htmlFor="Address">پست الکترونیکی فرستنده:</label>
-                      <input type="text" name="Address" className="form-control" title="The default email address to use as a sender." value={this.state.emailDetail.Address} onChange={this.submitHandleChange} />
+          </div>
+          <div className="todo-content">
+            <div className="portlet light">
+              <div className="portlet-title">
+                <div className="caption">
+                  <i className="fa fa-cogs font-green-sharp"></i>
+                  <span className="bold caption-subject font-green-sharp">حساب کاربری پست الکترونیکی</span>
+                </div>
+              </div>
+              <div className="portlet-body">
+                <div className="form-body">
+                  <div className="row">
+                    <input type="hidden" name="Id" value={this.state.emailDetail.Id} />
+                    <div className={`col-md-3 ${this.errorClass(this.state.emailDetail.errors.Address)}`}>
+                      <div className="form-group">
+                        <label htmlFor="Address">پست الکترونیکی فرستنده:</label>
+                        <input type="text" name="Address" className="form-control" title="The default email address to use as a sender." value={this.state.emailDetail.Address} onChange={this.submitHandleChange} />
+                      </div>
+                    </div>
+                    <div className={`col-md-3 ${this.errorClass(this.state.emailDetail.errors.Host)}`}>
+                      <div className="form-group">
+                        <label htmlFor="Host">سرور میزبان:</label>
+                        <input type="input" name="Host" className="form-control" title="The SMTP server domain, e.g. smtp.mailprovider.com." value={this.state.emailDetail.Host} onChange={this.submitHandleChange} />
+                      </div>
+                    </div>
+                    <div className={`col-md-3 ${this.errorClass(this.state.emailDetail.errors.Port)}`}>
+                      <div className="form-group">
+                        <label htmlFor="Port">شماره پورت:</label>
+                        <input type="text" name="Port" className="form-control" title="The SMTP server port, usually 25." value={this.state.emailDetail.Port} onChange={this.submitHandleChange} />
+                      </div>
+                    </div>
+                    <div className={`col-md-3 ${this.errorClass(this.state.emailDetail.errors.EnableSsl)}`}>
+                      <div className="form-group">
+                        <label htmlFor="EnableSsl">ارتباط SSL:</label>
+                        <select name="EnableSsl" className="form-control select2me" title="Check if the SMTP server requires SSL communications" value={this.state.emailDetail.EnableSsl} onChange={() => this.submitHandleChange}>
+                          <option value="true">بله</option>
+                          <option value="false">خیر</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className={`col-md-3 ${this.errorClass(this.state.emailDetail.errors.Status)}`}>
+                      <div className="form-group">
+                        <label htmlFor="Status">وضعیت سرویس تست:</label>
+                        <select name="Status" className="form-control select2me" value={this.state.emailDetail.Status} onChange={this.submitHandleChange}>
+                          <option value="true">فعال</option>
+                          <option value="false">غیرفعال</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className={`col-md-3 ${this.errorClass(this.state.emailDetail.errors.UserName)}`}>
+                      <div className="form-group">
+                        <label htmlFor="UserName">نام کاربری:</label>
+                        <input type="text" name="UserName" className="form-control" title="The username for authentication." value={this.state.emailDetail.UserName} onChange={this.submitHandleChange} />
+                      </div>
+                    </div>
+                    <div className={`col-md-6 ${this.errorClass(this.state.emailDetail.errors.Password)}`}>
+                      <div className="form-group">
+                        <label htmlFor="Password">رمز عبور:</label>
+                        <div className="input-group">
+                          <input type="password" name="Password" className="form-control" title="The password for authentication." value={this.state.emailDetail.Password} onChange={this.submitHandleChange} />
+                          <span className="input-group-btn" onClick={this.submitEmailAccountInformation4Save}>
+                            <button className="btn btn-success" disabled={!this.state.canSubmit} type="button">
+                              <i className="fa fa-save"></i> ذخیره
+                          </button>
+                          </span>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                  <div className={`col-md-3 ${this.errorClass(this.state.emailDetail.errors.Host)}`}>
-                    <div className="form-group">
-                      <label htmlFor="Host">سرور میزبان:</label>
-                      <input type="input" name="Host" className="form-control" title="The SMTP server domain, e.g. smtp.mailprovider.com." value={this.state.emailDetail.Host} onChange={this.submitHandleChange} />
+                </div>
+              </div>
+            </div>
+            <div className={`alert alert-warning alert-dismissable ${this.state.testResultMessage === "" ? "hidden" : ""}`}>
+              {this.state.testResultMessage}
+            </div>
+            <div className="portlet light">
+              <div className="portlet-title">
+                <div className="caption">
+                  <i className="fa fa-cogs font-green-sharp"></i>
+                  <span className="bold caption-subject font-green-sharp">تست سرویس پست الکترونیکی</span>
+                </div>
+              </div>
+              <div className="portlet-body">
+                <div className="form-body">
+                  <div className="row">
+                    <div className="col-md-3">
+                      <div className="form-group">
+                        <label htmlFor="To">پست الکترونیکی به:</label>
+                        <input type="text" name="To" className="form-control" value={this.state.testDetail.To} onChange={this.submitTestHandleChange} />
+                      </div>
                     </div>
-                  </div>
-                  <div className={`col-md-3 ${this.errorClass(this.state.emailDetail.errors.Port)}`}>
-                    <div className="form-group">
-                      <label htmlFor="Port">شماره پورت:</label>
-                      <input type="text" name="Port" className="form-control" title="The SMTP server port, usually 25." value={this.state.emailDetail.Port} onChange={this.submitHandleChange} />
+                    <div className="col-md-3">
+                      <div className="form-group">
+                        <label htmlFor="Subject">عنوان:</label>
+                        <input type="text" name="Subject" className="form-control" value={this.state.testDetail.Subject} onChange={this.submitTestHandleChange} />
+                      </div>
                     </div>
-                  </div>
-                  <div className={`col-md-3 ${this.errorClass(this.state.emailDetail.errors.EnableSsl)}`}>
-                    <div className="form-group">
-                      <label htmlFor="EnableSsl">ارتباط SSL:</label>
-                      <select name="EnableSsl" className="form-control select2me" title="Check if the SMTP server requires SSL communications" value={this.state.emailDetail.EnableSsl} onChange={() => this.submitHandleChange}>
-                        <option value="true">بله</option>
-                        <option value="false">خیر</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className={`col-md-3 ${this.errorClass(this.state.emailDetail.errors.Status)}`}>
-                    <div className="form-group">
-                      <label htmlFor="Status">وضعیت سرویس تست:</label>
-                      <select name="Status" className="form-control select2me" value={this.state.emailDetail.Status} onChange={this.submitHandleChange}>
-                        <option value="true">فعال</option>
-                        <option value="false">غیرفعال</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className={`col-md-3 ${this.errorClass(this.state.emailDetail.errors.UserName)}`}>
-                    <div className="form-group">
-                      <label htmlFor="UserName">نام کاربری:</label>
-                      <input type="text" name="UserName" className="form-control" title="The username for authentication." value={this.state.emailDetail.UserName} onChange={this.submitHandleChange} />
-                    </div>
-                  </div>
-                  <div className={`col-md-6 ${this.errorClass(this.state.emailDetail.errors.Password)}`}>
-                    <div className="form-group">
-                      <label htmlFor="Password">رمز عبور:</label>
+                    <div className="col-md-6">
+                      <label htmlFor="Message">متن پیام:</label>
                       <div className="input-group">
-                        <input type="password" name="Password" className="form-control" title="The password for authentication." value={this.state.emailDetail.Password} onChange={this.submitHandleChange} />
-                        <span className="input-group-btn" onClick={this.submitEmailAccountInformation4Save}>
-                          <button className="btn btn-success" disabled={!this.state.canSubmit} type="button">
-                            <i className="fa fa-save"></i> ذخیره
+                        <input type="text" name="Message" className="form-control" value={this.state.testDetail.Message} onChange={this.submitTestHandleChange} />
+                        <span className="input-group-btn" onClick={this.submitEmailAccountInformation4Test}>
+                          <button className="btn btn-primary" disabled={!(this.state.canSubmit && this.state.allowToTest)} type="button">
+                            <i className="fa fa-arrow-left fa-fw"></i>
+                            تســـت
                           </button>
                         </span>
                       </div>
@@ -334,49 +398,8 @@ class App extends Component {
                 </div>
               </div>
             </div>
+            <this.renderTable parent={this} onDeleteEmail={this.onDeleteEmail} onEditEmail={this.onEditEmail} />
           </div>
-          <div className={`alert alert-warning alert-dismissable ${this.state.testResultMessage === "" ? "hidden" : ""}`}>
-            {this.state.testResultMessage}
-          </div>
-          <div className="portlet light">
-            <div className="portlet-title">
-              <div className="caption">
-                <i className="fa fa-cogs font-green-sharp"></i>
-                <span className="bold caption-subject font-green-sharp">تست سرویس پست الکترونیکی</span>
-              </div>
-            </div>
-            <div className="portlet-body">
-              <div className="form-body">
-                <div className="row">
-                  <div className="col-md-3">
-                    <div className="form-group">
-                      <label htmlFor="To">پست الکترونیکی به:</label>
-                      <input type="text" name="To" className="form-control" value={this.state.testDetail.To} onChange={this.submitTestHandleChange} />
-                    </div>
-                  </div>
-                  <div className="col-md-3">
-                    <div className="form-group">
-                      <label htmlFor="Subject">عنوان:</label>
-                      <input type="text" name="Subject" className="form-control" value={this.state.testDetail.Subject} onChange={this.submitTestHandleChange} />
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <label htmlFor="Message">متن پیام:</label>
-                    <div className="input-group">
-                      <input type="text" name="Message" className="form-control" value={this.state.testDetail.Message} onChange={this.submitTestHandleChange} />
-                      <span className="input-group-btn" onClick={this.submitEmailAccountInformation4Test}>
-                        <button className="btn btn-primary" disabled={!(this.state.canSubmit && this.state.allowToTest)} type="button">
-                          <i className="fa fa-arrow-left fa-fw"></i>
-                          تســـت
-                          </button>
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <this.renderTable parent={this} onDeleteEmail={this.onDeleteEmail} onEditEmail={this.onEditEmail} />
         </div>
       </div>
     );
